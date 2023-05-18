3bf8b9c6af3bc379e4c8539bc35eb07465dd1a2b
OFFME DoD <offmemyg@gmail.com>
2023-05-18 23:58:01 +0300

    add description/add error handler

--- RPI-social__survey/Form1.cs
+++ RPI-social__survey/Form1.cs
@@ -7,7 +7,7 @@ namespace RPI_social__survey
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("Ns lfey");
+            MessageBox.Show("HELLO THIS SOCIAL SURVEY IS CREATED FOR STUDENTS FROM OTHER COUNTRIES LIVING IN THE CITY OF KAZAN. QUESTIONS VERY SIMPLE\r\n--PLEASE COMPLETE ALL COLUMNS--");
         }
 
         public string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\offme\\Documents\\SOCIAL.mdf;Integrated Security=True;Connect Timeout=30";
@@ -19,22 +19,29 @@ namespace RPI_social__survey
 
         private void SEND_Click(object sender, EventArgs e)
         {
-
-            SqlConnection con = new SqlConnection(conString);
-
-            con.Open();
-
-            if (con.State == System.Data.ConnectionState.Open)
+            try
             {
-                string q = "insert into [Table](name_univer, course," +
-                  " perfomance, work, country, sty, doafter)" +
-                  " values('" + textBox1.Text.ToString() + "','" + textBox2.Text.ToString() + "','" + textBox5.Text.ToString() + "','" + comboBox4.Text.ToString() + "','" + textBox4.Text.ToString() + "','" + comboBox2.Text.ToString() + "','" + textBox3.Text.ToString() + "') ";
-                SqlCommand cmd = new SqlCommand(q, con);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Connection was successful!");
-            }
+                SqlConnection con = new SqlConnection(conString);
 
-            con.Close();
+                con.Open();
+
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    string q = "insert into [Table](name_univer, course," +
+                      " perfomance, work, country, sty, doafter)" +
+                      " values('" + textBox1.Text.ToString() + "','" + textBox2.Text.ToString() + "','" + textBox5.Text.ToString() + "','" + comboBox4.Text.ToString() + "','" + textBox4.Text.ToString() + "','" + comboBox2.Text.ToString() + "','" + textBox3.Text.ToString() + "') ";
+                    SqlCommand cmd = new SqlCommand(q, con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Connection was successful!");
+                }
+
+                con.Close();
+            }
+            catch
+            {
+                MessageBox.Show("columns are empty");
+            }
+            
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
